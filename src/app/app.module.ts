f47201cf92782eb405392433585357593b10afbf
Kylie <krchristensen94@gmail.com>
2019-10-30 23:03:32 -0500

    It do search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {ReactiveFormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
+import { OrbitListComponent } from './orbit-list/orbit-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrbitListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
